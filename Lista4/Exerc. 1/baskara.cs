bad955cbdd9fa337e78776e3e458677a63981392
Peku2 <148865987+Peku2@users.noreply.github.com>
2024-03-15 10:07:10 -0300

    Adicionar Atividades

---
+++ Lista4/Exerc. 1/baskara.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+namespace Calculadora
+{
+    class PRogram
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, x1, x2, delta;
+
+            Console.WriteLine("Digite o valor de A: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
